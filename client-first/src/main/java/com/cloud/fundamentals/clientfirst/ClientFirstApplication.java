2ba936632fe82024298c0e0985377afc6835669a
swastiksuman <swastiksuman@gmail.com>
2021-03-22 21:52:53 +0530

    client app to swagger

--- client-first/src/main/java/com/cloud/fundamentals/clientfirst/ClientFirstApplication.java
+++ client-first/src/main/java/com/cloud/fundamentals/clientfirst/ClientFirstApplication.java
@@ -3,6 +3,11 @@ package com.cloud.fundamentals.clientfirst;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -11,5 +16,10 @@ public class ClientFirstApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientFirstApplication.class, args);
 	}
-
+	
+	@Bean
+	   public Docket productApi() {
+	      return new Docket(DocumentationType.SWAGGER_2).select()
+					.apis(RequestHandlerSelectors.basePackage("com.cloud.fundamentals.clientfirst")).build();
+	   }
 }
