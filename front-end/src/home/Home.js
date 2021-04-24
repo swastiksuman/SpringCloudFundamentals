6253a660f52456f9fc2bacefa665a41ae4b8fa07
Swastik Panda <swastiksuman@gmail.com>
2021-04-24 23:29:16 +0530

    moving the component

--- front-end/src/home/Home.js
+++ front-end/src/home/Home.js
@@ -6,9 +6,7 @@ import Search from './search/Search';
 function Home() {
 
     const [result, setResult] = useState(null);
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [itemId, setItemId] = useState('');
+    
 
 
     function doTest() {
@@ -21,32 +19,9 @@ function Home() {
         console.log('***Test***');
     }
 
-    function placeOrder() {
-        const placeOrder = {
-            firstName: "Soujanya",
-            lastName: "Talla",
-            itemId: "123"
-        };
-        axios.post('http://localhost:8080/placeOrder', {
-            firstName: firstName,
-            lastName: lastName,
-            itemId: itemId
-        })
-            .then(response => console.log(response));
-    }
-
     return (
         <div>
             <Search></Search>
-            <div>
-                <label>First Name</label><input
-                    onChange={(e) => setFirstName(e.target.value)} type="text" />
-                <label>Last Name</label><input
-                    onChange={(e) => setLastName(e.target.value)} type="text" />
-                <label>Item Id</label><input
-                    onChange={(e) => setItemId(e.target.value)} type="text" />
-                <Button variant="primary" onClick={() => placeOrder()}>Submit Order</Button>
-            </div>
         </div>
     )
 }
--- front-end/src/home/search/Search.js
+++ front-end/src/home/search/Search.js
@@ -5,9 +5,27 @@ import { Col, Container, Row } from "react-bootstrap";
 
 function Search(){
     const [result, setResult] = useState(null);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [itemId, setItemId] = useState('');
+
     function doSearch(){
         axios.get('http://localhost:8081/searchProduct')
             .then(res=>setResult(res.data.searchResult));
+    }
+
+    function placeOrder() {
+        const placeOrder = {
+            firstName: "Soujanya",
+            lastName: "Talla",
+            itemId: "123"
+        };
+        axios.post('http://localhost:8080/placeOrder', {
+            firstName: firstName,
+            lastName: lastName,
+            itemId: itemId
+        })
+            .then(response => console.log(response));
     }
     return (
         <div>
@@ -48,6 +66,15 @@ function Search(){
                 }
                 </Row>
             </Container>
+            <div>
+                <label>First Name</label><input
+                    onChange={(e) => setFirstName(e.target.value)} type="text" />
+                <label>Last Name</label><input
+                    onChange={(e) => setLastName(e.target.value)} type="text" />
+                <label>Item Id</label><input
+                    onChange={(e) => setItemId(e.target.value)} type="text" />
+                <Button variant="primary" onClick={() => placeOrder()}>Submit Order</Button>
+            </div>
         </div>
     )
 }
